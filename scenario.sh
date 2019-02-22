4b0927c30cc41ab2c1abc8763f75e9b91f92c1bc
Mykola Malyna <malyna.mykola95@gmail.com>
2019-02-22 11:23:01 +0200

    Іnstall Moodle by php-cli

--- scenario.sh
+++ scenario.sh
@@ -34,32 +34,6 @@ sudo yum -y install wget
 sudo wget https://download.moodle.org/download.php/direct/stable36/moodle-latest-36.tgz
 tar -zxvf moodle-latest-36.tgz
 sudo cp -R moodle /var/www/html
-sudo mkdir /var/moodledata
-sudo chmod 777 /var/moodledata/
-sudo touch /var/www/html/moodle/config.php
-sudo chmod 777 /var/www/html/moodle/config.php
-sudo echo "<?php  // Moodle configuration file
-
-unset(\$CFG);
-global \$CFG;
-\$CFG = new stdClass();
-\$CFG->dbtype    = 'mariadb';
-\$CFG->dblibrary = 'native';
-\$CFG->dbhost    = 'localhost';
-\$CFG->dbname    = 'dbname';
-\$CFG->dbuser    = 'mykola';
-\$CFG->dbpass    = '123456';
-\$CFG->prefix    = 'mdl_';
-\$CFG->dboptions = array (
-  'dbpersist' => 0,
-  'dbport' => 3306,
-  'dbsocket' => '',
-  'dbcollation' => 'utf8mb4_general_ci',
-);
-\$CFG->wwwroot   = 'http://192.168.56.2/moodle';
-\$CFG->dataroot  = '/var/moodledata';
-\$CFG->admin     = 'admin';
-
-\$CFG->directorypermissions = 0777;
-
-require_once(__DIR__ . '/lib/setup.php');" > /var/www/html/moodle/config.php
+sudo /usr/bin/php /var/www/html/moodle/admin/cli/install.php --wwwroot=http://192.168.56.2/moodle --dataroot=/var/moodledata --dbtype=mariadb --dbname=dbname --dbuser=mykola --dbpass=123456 --fullname="Moodle" --adminpass=admin  --shortname="Moodle" --non-interactive --agree-license
+sudo chmod a+r /var/www/html/moodle/config.php
+sudo chcon -R -t httpd_sys_rw_content_t /var/moodledata
