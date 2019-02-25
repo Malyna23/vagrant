a88ab0ba6dd6330f9669824e921e18fccd223f3d
Malyna23 <46026189+Malyna23@users.noreply.github.com>
2019-02-24 22:22:41 -0800

    Change password php cli

--- scenario.sh
+++ scenario.sh
@@ -34,6 +34,6 @@ sudo yum -y install wget
 sudo wget https://download.moodle.org/download.php/direct/stable36/moodle-latest-36.tgz
 tar -zxvf moodle-latest-36.tgz
 sudo cp -R moodle /var/www/html
-sudo /usr/bin/php /var/www/html/moodle/admin/cli/install.php --wwwroot=http://192.168.56.2/moodle --dataroot=/var/moodledata --dbtype=mariadb --dbname=dbname --dbuser=mykola --dbpass=123456 --fullname="Moodle" --adminpass=admin  --shortname="Moodle" --non-interactive --agree-license
+sudo /usr/bin/php /var/www/html/moodle/admin/cli/install.php --wwwroot=http://192.168.56.2/moodle --dataroot=/var/moodledata --dbtype=mariadb --dbname=dbname --dbuser=mykola --dbpass=123456 --fullname="Moodle" --adminpass=1Qaz2wsx$  --shortname="Moodle" --non-interactive --agree-license
 sudo chmod a+r /var/www/html/moodle/config.php
 sudo chcon -R -t httpd_sys_rw_content_t /var/moodledata
